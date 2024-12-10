9d83e523084b4c17a04fc019d8d22830977d5e27
LintangID <lintangadi51@gmail.com>
2024-12-10 18:21:28 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -11,13 +11,12 @@ Route::middleware(['auth:sanctum'])->group(function(){
     Route::get('/checklists',[ChecklistController::class, 'index']);
     Route::post('/checklists',[ChecklistController::class, 'store']);
     Route::delete('/checklists/{id}',[ChecklistController::class, 'destroy']);
+    Route::get('/checklists/{checklistId}/item',[ItemController::class, 'index']);
+    Route::post('/checklists/{checklistId}/item',[ItemController::class, 'store']);
+    Route::get('/checklists/{checklistId}/item/{id}',[ItemController::class, 'show']);
+    Route::put('/checklists/{checklistId}/item/{id}',[ItemController::class, 'update']);
+    Route::delete('/checklists/{checklistId}/item/{id}',[ItemController::class, 'destroy']);
+    Route::put('/checklists/{checklistId}/item/rename/{id}',[ItemController::class, 'rename']);
 });
-
-Route::get('/checklists/{checklistId}/item',[ItemController::class, 'index']);
-Route::post('/checklists/{checklistId}/item',[ItemController::class, 'store']);
-Route::get('/checklists/{checklistId}/item/{id}',[ItemController::class, 'show']);
-Route::put('/checklists/{checklistId}/item/{id}',[ItemController::class, 'update']);
-Route::delete('/checklists/{checklistId}/item/{id}',[ItemController::class, 'destroy']);
-Route::put('/checklists/{checklistId}/item/rename/{id}',[ItemController::class, 'rename']);
 Route::post('/login',[AuthenticationController::class, 'login']);
 Route::post('/register',[AuthenticationController::class, 'register']);
